9d78388609d6cdb5e713bf75b4602c8731f29f6e
Saravana <saravanakumar@nfnlabs.in>
2018-12-30 19:47:39 +0530

    A sample keyboard binding is added

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
+
+var nodeConsole = require('console');
+var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+myConsole.log('Hello World!');
+
 var quill = require('quill');
 var editor = new quill('#editor',{
+	theme:'snow',
 	modules: {
 		// toolbar : {container: '#toolbar'},
 		// 'image-tooltip':true,
+		// keyboard: {
+  //     bindings: bindings
+  //   }
 	},
-	placeholder: 'Compose an epic...',
-	theme:'snow'
+	placeholder: 'Compose an epic...'
+	
 });
 
 
-var fileSystem = require('fs');
-var remote = require('electron').remote;
-var dialog = remote.require('electron').dialog;
+editor.keyboard.addBinding({
+  key: 'B'
+  // shortKey: true
+}, function(range, context) {
+  myConsole.log("fkajlsdfadsljfajlsd");
+  myConsole.log(range);
+  myConsole.log(context);
+  this.editor.formatText(range, 'bold', true);
+});
 
-var loadedFileSystem;
 
-function writeToFile(editor, fileName) {
-	var html = editor.getHTML();
-	fileSystem.writeFile(fileName,html,function err() {
-		if (err) {
-			return console.log(err);
-		}
-	});
-}
+// var fileSystem = require('fs');
+// var remote = require('electron').remote;
+// var dialog = remote.require('electron').dialog;
 
-function readFromFile(editor, fileName) {
-	fileSystem.readFile(fileName,"utf-8",function(err,data) {
-		if (err) {
-			console.log(err);
-		}
-		editor.setHTML(data);
-	});
-}
+// var loadedFileSystem;
+
+// function writeToFile(editor, fileName) {
+// 	var html = editor.getHTML();
+// 	fileSystem.writeFile(fileName,html,function err() {
+// 		if (err) {
+// 			return console.log(err);
+// 		}
+// 	});
+// }
+
+// function readFromFile(editor, fileName) {
+// 	fileSystem.readFile(fileName,"utf-8",function(err,data) {
+// 		if (err) {
+// 			console.log(err);
+// 		}
+// 		editor.setHTML(data);
+// 	});
+// }
